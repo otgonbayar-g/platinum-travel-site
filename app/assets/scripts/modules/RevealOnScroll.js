e925b133cb44b69ee63d9c8d56f1c96b7d9809f2
Otgonbayar <otgonbayar.g@gmail.com>
2023-01-10 14:02:45 +0800

    Sticky header-g buren hiij duusgalaa

--- app/assets/scripts/modules/RevealOnScroll.js
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -28,7 +28,7 @@ class RevealOnScroll {
 
     calculateIfScrolledTo(el) {
         if (window.scrollY + this.browserHeight > el.offsetTop) {
-            let scrollPercent = (el.getBoundingClientRect().y / this.browserHeight) * 100
+            let scrollPercent = (el.getBoundingClientRect().y / this.browserHeight) * 100;
             if (scrollPercent < this.revealPoint) {
                 el.classList.add('reveal-item--is-visible');
                 el.isRevealed = true;
