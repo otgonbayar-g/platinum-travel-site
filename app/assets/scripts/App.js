b066a1fff0e20de0741dd28fcb5ccfde0a36de80
Otgonbayar <otgonbayar.g@gmail.com>
2023-01-19 13:16:12 +0800

    Modal overlay-g buren hiij duuslaa

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,12 +3,28 @@ import MobileMenu from "./modules/MobileMenu";
 import RevealOnScroll from "./modules/RevealOnScroll";
 import StickyHeader from "./modules/StickyHeader";
 
-let stickyHeader = new StickyHeader();
-
+new StickyHeader();
 new RevealOnScroll(document.querySelectorAll('.feature-item'), 75);
 new RevealOnScroll(document.querySelectorAll('.testimonial'), 60);
+new MobileMenu();
 
-let mobileMenu = new MobileMenu();
+let modal;
+
+document.querySelectorAll('.open-modal').forEach(el => {
+    el.addEventListener('click', e => {
+        e.preventDefault();
+        if (typeof modal == 'undefined') {
+            import(/*webpackChunkName: 'modal' */ './modules/Modal')
+                .then(x => {
+                    modal = new x.default();
+                    setTimeout(() => modal.openModal(), 20);
+                })
+                .catch(() => console.log('Модалыг дуудахад алдаа гарлаа!'));
+        } else {
+            modal.openModal();
+        }
+    });
+});
 
 if (module.hot) {
     module.hot.accept();
